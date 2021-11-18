2bb7bb0e6f153da5898c3f92a09b87b3cc7be852
guoconde <guoconde@gmail.com>
2021-11-18 13:28:43 -0300

    Implementação Tela criação quizz / Layout para seus quizzes

--- js/main.js
+++ js/main.js
@@ -35,25 +35,25 @@ function openQuizz(element) {
     const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${element}`)
     promise.then(renderQuiz)
 }
-function renderQuiz(response){
+function renderQuiz(response) {
     const data = response.data;
     const mainImage = data.image;
     const title = data.title;
 
     const showImage = document.querySelector(".picture");
     const Quizz = document.querySelector(".opened-quizz")
-    
+
     showImage.innerHTML = `
         <p class="title">${title}</p>
         <img src="${mainImage}">
     `
-    for (let i = 0; i < data.questions.length; i++){
+    for (let i = 0; i < data.questions.length; i++) {
         Quizz.innerHTML += `
         <div class="content">
             <p class="title" style="background-color: ${data.questions[i].color};">${data.questions[i].title} </p>
             
             `
-        for (let j = 0; j < data.questions[i].answers.length; j++){
+        for (let j = 0; j < data.questions[i].answers.length; j++) {
             const answer = data.questions[i].answers[j];
             Quizz.innerHTML += `
                 <div class="answers"> 
@@ -66,6 +66,15 @@ function renderQuiz(response){
         }
     }
 }
+
 function createQuizz() {
-    console.log('Quizz Criado')
+    const main = document.querySelector('.main')
+    const creatingQuizz = document.querySelector('.creating-quizz')
+
+    main.classList.add('hide')
+    creatingQuizz.classList.remove('hide')
+}
+
+function next() {
+    console.log('proximo')
 }
