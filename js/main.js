74ff302903d8ce13ace080d60cfe019517739a48
guoconde <guoconde@gmail.com>
2021-11-17 23:32:07 -0300

    Link axios / GET quizz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('foi')
+const getThumbnails = document.querySelector('.thumbnails')
+
+axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+    .then(el => {
+        getThumbnails.innerHTML = ''
+
+        console.log(el.data)
+
+        for (let i = 0; i < el.data.length; i++) {
+            getThumbnails.innerHTML += `
+                <div class="thumbnail" onclick="openQuizz(this)">
+                    <img src="${el.data[i].image}">
+                    <h2>${el.data[i].title}</h2>
+                </div>
+            `
+        }
+    })
+    .catch(() => {
+        console.log('deu erro no get1')
+    })
+
+function openQuizz(element) {
+    console.log(element)
+}
+
+function createQuizz() {
+    console.log('Quizz Criado')
+}
