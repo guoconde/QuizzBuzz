6e1c9b719ea5cf560c07ed5269bd472f7d8f13ea
Kayque Coelho <lkcoelho76@gmail.com>
2021-11-18 09:13:29 -0300

    exibindo conteúdo do quizz[C

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
 
         for (let i = 0; i < el.data.length; i++) {
             getThumbnails.innerHTML += `
-                <div class="thumbnail" onclick="openQuizz(this)">
+                <div class="thumbnail" onclick="openQuizz(${el.data[i].id})">
                     <img src="${el.data[i].image}">
                     <h2>${el.data[i].title}</h2>
                 </div>
@@ -31,8 +31,31 @@ function openQuizz(element) {
 
     showQuizz.classList.remove('hide')
     showQuizz.classList.add('show')
-}
 
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${element}`)
+    promise.then(renderQuiz)
+}
+function renderQuiz(response){
+    const data = response.data;
+    const mainImage = data.image;
+    const title = data.title;
+
+    const showImage = document.querySelector(".picture");
+    const Quizz = document.querySelector(".opened-quizz")
+
+    showImage.innerHTML = `
+        <p class="title">${title}</p>
+        <img src="${mainImage}">
+    `
+    for (let i = 0; i < data.questions.length; i++){
+        Quizz.innerHTML += `
+        <div class="content">
+            <p class="title">${data.questions[i].title}</p>
+            <div class="answers"> 
+            </div>
+        </div>`
+    }
+}
 function createQuizz() {
     console.log('Quizz Criado')
 }
